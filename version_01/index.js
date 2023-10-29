befc4046849644a18065b79bd64fcb1f69c2d52d
salinpaul valooran <salinpv@hotmail.com>
2023-10-29 13:08:59 +0530

    variables and on click listener for generating qr code

--- version_01/index.js
+++ version_01/index.js
@@ -1,5 +1,9 @@
 const qrCodeApi = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
 
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
 function generateQR() {
 
 }
